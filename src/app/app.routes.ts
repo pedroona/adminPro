209d68dd0cf4ea151d2ec423693e512369e0a246
Pedro Pablo Oña Leiva <pedropabloonaleiva@gmail.com>
2018-12-19 12:29:45 +0100

    Primer commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,29 @@
+import { Routes, RouterModule } from '@angular/router';
+
+import { PagesComponent } from './pages/pages.component';
+
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { PagenotfoundComponent } from './shared/pagenotfound/pagenotfound.component';
+import { LoginComponent } from './login/login.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { Graficas1Component } from './pages/graficas1/graficas1.component';
+import { RegisterComponent } from './login/register.component';
+
+const ROUTES: Routes = [
+    {
+        path: '',
+        component: PagesComponent,
+        children: [
+            { path: '', pathMatch: 'full', redirectTo: '/dashboard' },
+            { path: 'dashboard', component: DashboardComponent },
+            { path: 'progress', component: ProgressComponent },
+            { path: 'graficas1', component: Graficas1Component },
+        ]
+     },
+    { path: 'login', component: LoginComponent },
+    { path: 'register', component: RegisterComponent },
+    { path: '**', component: PagenotfoundComponent }
+];
+
+
+export const APP_ROUTING = RouterModule.forRoot(ROUTES, {useHash: true});
